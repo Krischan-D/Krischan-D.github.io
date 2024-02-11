73f5d21b29e7293ccdd25018a9f211aded140ec7
Krischan-D <159700908+Krischan-D@users.noreply.github.com>
2024-02-12 00:26:56 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const Homebtn = document.getElementById("HomeBtn");
+const AboutBtn = document.getElementById("AboutBtn");
+const ContactBtn = document.getElementById("ContactBtn");
+
+Homebtn.onclick = function(){
+    window.alert(`You are already in the home page`);
+}
+
+AboutBtn.onclick = function(){
+    window.alert(`This website is an invitation for you`);
+}
+
+ContactBtn.onclick = function(){
+    window.alert(`Your babie, echan`);
+}
